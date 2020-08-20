7974f3675e94ac45f3e9e568697d0a6be5097a5d
Edgar Garcia <edgargarcia2294@gmail.com>
2020-08-20 08:56:07 -0500

    Algunos cambios 🤔

--- main.c
+++ main.c
@@ -11,24 +11,24 @@ typedef struct {
 	char* apellidos;
 	int edad;
 	char* telefono;
-} paciente;
+} persona;
 
 typedef struct {
 	int id;
-	paciente* p;
+	persona* p;
 } cama;
 
 //funciones 
 int agregarCamas(cama** camas, int** disponibles, int totalCamas, int* totalDisponibles, int n);
-void agregarPaciente(cama* camas, int id, int maxLen);
+void agregarPersona(cama* camas, int id, int maxLen);
 int agregarDisponible(int** disponibles, int id, int total);
 int encontrarDisponible(int** disponibles, int* total);
 
 int buscarCama(int total);
 void imprimirCama(cama* camas, int id);
-void listarPacientes(cama* camas, int total);
+void listarPersonas(cama* camas, int total);
 
-void altaPaciente(cama* camas, int id, int** disponibles, int* totalDisponibles);
+void altaPersona(cama* camas, int id, int** disponibles, int* totalDisponibles);
 void liberarCamas(cama* camas, int total);
 char* obtenerDatos(char* str, int maxLen);
 
@@ -45,7 +45,7 @@ int main() {
 
 	//Menu 
 	do {
-		printf("\n1. Incorporar paciente\n2. Buscar cama\n3. Dar de alta a paciente\n4. Lista de pacientes\n5. Ver camas disponibles\n0. Salir \nSelecciona una opcion: ");
+		printf("\n1. Incorporar persona\n2. Buscar cama\n3. Dar de alta a la persona\n4. Lista de personas\n5. Ver camas disponibles\n0. Salir \nSelecciona una opcion: ");
 		scanf("%d", &opcion);
 		getchar();
 
@@ -56,7 +56,7 @@ int main() {
 					totalCamas = agregarCamas(&camas, &disponibles, totalCamas, &totalDisponibles, 5);
 				}
 				id = encontrarDisponible(&disponibles, &totalDisponibles);
-				agregarPaciente(camas, id, maxLen);
+				agregarPersona(camas, id, maxLen);
 				break;
 			case 2:
 				id = buscarCama(totalCamas);
@@ -66,10 +66,10 @@ int main() {
 			case 3:
 				id = buscarCama(totalCamas);
 				if (id != -1)
-					altaPaciente(camas, id, &disponibles, &totalDisponibles);
+					altaPersona(camas, id, &disponibles, &totalDisponibles);
 				break;
 			case 4:
-				listarPacientes(camas, totalCamas);
+				listarPersonas(camas, totalCamas);
 				break;
 			case 5:
 				printf("Camas disponibles: %d\nCamas ocupadas: %d\n", totalDisponibles, totalCamas - totalDisponibles);
@@ -100,12 +100,12 @@ int agregarCamas(cama** camas, int** disponibles, int totalCamas, int* totalDisp
 	return totalCamas;
 }
 
-//Agregar Paciente nuevo
-void agregarPaciente(cama* camas, int id, int maxLen) {
+//Agregar persona nuevo
+void agregarPersona(cama* camas, int id, int maxLen) {
 	printf("Cama %d\n", id);
 	cama* c = camas + id - 1;
 
-	c->p = (paciente*) malloc(sizeof(paciente));
+	c->p = (persona*) malloc(sizeof(persona));
 	c->p->nombre = (char*) malloc(maxLen * sizeof(char));
 	c->p->apellidos = (char*) malloc(maxLen * sizeof(char));
 	c->p->telefono = (char*) malloc(maxLen * sizeof(char));
@@ -154,22 +154,22 @@ int buscarCama(int total) {
 void imprimirCama(cama* camas, int id) {
 	cama* c = camas + id - 1;
 	if (c->p)
-		printf("Paciente: %s %s, %d años, Tel: %s\n", c->p->nombre, c->p->apellidos, c->p->edad, c->p->telefono);
+		printf("La persona: %s %s, %d años, Tel: %s\n", c->p->nombre, c->p->apellidos, c->p->edad, c->p->telefono);
 	else
 		printf("Cama disponible\n");
 }
 
-//Lista de los pacientes 
-void listarPacientes(cama* camas, int total) {
+//Lista de los personas 
+void listarPersonas(cama* camas, int total) {
 	cama* c;
 	for (c = camas; c < camas + total; ++c) {
 		if (c->p)
-			printf("Cama %d -> Paciente: %s %s, %d años, Tel: %s\n", c->id, c->p->nombre, c->p->apellidos, c->p->edad, c->p->telefono);
+			printf("Cama %d -> persona: %s %s, %d años, Tel: %s\n", c->id, c->p->nombre, c->p->apellidos, c->p->edad, c->p->telefono);
 	}
 }
 
-//Datos de paciente dado de alto
-void altaPaciente(cama* camas, int id, int** disponibles, int* totalDisponibles) {
+//Datos de persona dado de alto
+void altaPersona(cama* camas, int id, int** disponibles, int* totalDisponibles) {
 	cama* c = camas + id - 1;
 	free(c->p->nombre);
 	free(c->p->apellidos);
@@ -178,7 +178,7 @@ void altaPaciente(cama* camas, int id, int** disponibles, int* totalDisponibles)
 	c->p = NULL;
 
 	*totalDisponibles = agregarDisponible(disponibles, id, *totalDisponibles);
-	printf("El paciente fue dado de alta\n");
+	printf("La Persona fue dado de alta\n");
 }
 
 //Liberacion de camas
